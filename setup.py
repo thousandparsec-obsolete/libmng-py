8208fc94fd493f8ae5d0d981d4147f764aa2dc96
Tim Ansell <tim@thousandparsec.net>
2006-12-01 07:27:50 +0000

    Added setup.py file.

---
+++ setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import shutil
+import sys
+
+import glob
+import os.path
+
+from setuptools import setup
+
+from mng.version import version
+version = ("%s.%s.%s" % version) 
+print "Version is %s" % version
+
+arguments = dict(
+# Meta data
+	name		= "libmng-py",
+	version		= version,
+	license		= "GPL",
+	description	= "libmng for displaying MNG and JNG animations",
+	author		= "Tim Ansell",
+	author_email= "mithro@mithis.com",
+	url			= "http://www.thousandparsec.net/repos/libmng-py",
+# Files to include
+	scripts=["mngtree.py", "mngdisplay.py"],
+	packages=[ \
+		'mng',
+		'mng.pygame',
+		'mng.wx',
+		'mng.utils',
+	],
+	data_files=[(".",	("LICENSE", "COPYING", "README", "TODO"))],
+	zip_safe=False,
+)
+
+if sys.platform == "win32":
+	arguments['data_files'].append((os.path.join("mng", "dll"), "libmng.dll"))
+
+setup(arguments)
