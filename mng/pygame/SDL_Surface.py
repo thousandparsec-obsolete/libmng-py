877d99558d75c99da7b3855a981789ee1b320f02
Tim Ansell <tim@thousandparsec.net>
2006-12-03 14:18:43 +0000

    Much faster method for doing the surfaces.

--- mng/pygame/SDL_Surface.py
+++ mng/pygame/SDL_Surface.py
@@ -1,5 +1,62 @@
 from ctypes import *
 
+class SDL_PixelFormat(Structure):
+    '''Read-only surface format structure.
+
+    :Ivariables:
+        `BitsPerPixel` : int
+            Number of bits per pixel
+        `BytesPerPixel` : int
+            Number of bytes per pixel.  This is not necessarily 8 *
+            BitsPerPixel.
+        `Rloss` : int
+            Number of bits lost from an 8-bit red component
+        `Gloss` : int
+            Number of bits lost from an 8-bit green component
+        `Bloss` : int
+            Number of bits lost from an 8-bit blue component
+        `Aloss` : int
+            Number of bits lost from an 8-bit alpha component
+        `Rshift` : int
+            Number of bits to shift right red component when packing
+        `Gshift` : int
+            Number of bits to shift right green component when packing
+        `Bshift` : int
+            Number of bits to shift right blue component when packing
+        `Ashift` : int
+            Number of bits to shift right alpha component when packing
+        `Rmask` : int
+            32-bit mask of red component
+        `Gmask` : int
+            32-bit mask of green component
+        `Bmask` : int
+            32-bit mask of blue component
+        `Amask` : int
+            32-bit mask of alpha component
+        `colorkey` : int
+            Packed transparent color key, if set.
+        `alpha` : int
+            Surface alpha, in range [0, 255]
+
+    '''
+    _fields_ = [('_palette', c_void_p),
+                ('BitsPerPixel', c_ubyte),
+                ('BytesPerPixel', c_ubyte),
+                ('Rloss', c_ubyte),
+                ('Gloss', c_ubyte),
+                ('Bloss', c_ubyte),
+                ('Aloss', c_ubyte),
+                ('Rshift', c_ubyte),
+                ('Gshift', c_ubyte),
+                ('Bshift', c_ubyte),
+                ('Ashift', c_ubyte),
+                ('Rmask', c_uint),
+                ('Gmask', c_uint),
+                ('Bmask', c_uint),
+                ('Amask', c_uint),
+                ('colorkey', c_uint),
+                ('alpha', c_ubyte)]
+
 class SDLSurfaceStructure(Structure):
     '''Read-only surface structure.
 
@@ -24,7 +81,7 @@ class SDLSurfaceStructure(Structure):
             `SDL_LockSurface`.
     '''
     _fields_ = [('flags', c_uint),
-                ('format', c_void_p),
+                ('format', POINTER(SDL_PixelFormat)),
                 ('w', c_int),
                 ('h', c_int),
                 ('pitch', c_short),
@@ -52,9 +109,9 @@ class CPygameSurface:
 
 	def __getattr__(self, key):
 		if hasattr(self.pointer.contents.surf[0], key):
-			if key in self.mapping:
-				return self.mapping[key](getattr(self.pointer.contents.surf[0], key))
-			else:
+#			if key in self.mapping:
+#				return self.mapping[key](getattr(self.pointer.contents.surf[0], key))
+#			else:
 				return getattr(self.pointer.contents.surf[0], key)
 		raise KeyError("No such key (%s) exists" % key)
 
--- mng/pygame/pygameMNG.py
+++ mng/pygame/pygameMNG.py
@@ -2,23 +2,7 @@
 from ctypes import *
 
 import pygame
-screen = pygame.display.set_mode((640, 480), 16, 16) 
-pygame.display.set_caption('Battle Viewer') 
-screen.fill((255,255,255))
-
-s1 = pygame.surface.Surface((100,100))
-
 from SDL_Surface import CPygameSurface
-s = CPygameSurface(s1)
-
-print s.flags
-print s.format
-print s.w
-print s.h
-print s.pitch
-print s.pixels
-
-import pygame
 
 from mng import MNG
 from constants import *
@@ -31,17 +15,30 @@ class pygameMNG(MNG):
 	"""
 	def __init__(self, file, surface=None):
 		# Figure out the best fileformat for the library to output
-		if surface is None:
-			info = pygame.display.Info()
-		else:
-			class info:
-				pass
-			info = info()
-			info.bitsize = surface.get_bitsize()
-			info.masks = surface.get_masks()
-			info.shifts = surface.get_shifts()
+		class info:
+			def __str__(self):
+				return "<Info b:%s m:%s s:%s l:%s>" % (self.bitsize, self.masks, self.shifts, self.losses)
 
-		print info.bitsize, info.masks, info.shifts	
+		info = info()
+		if surface is None:
+			i = pygame.display.Info()
+			info.masks = i.masks
+			info.shifts = i.shifts
+			info.losses = i.losses
+
+			info.bitsize = i.bitsize
+			# FIXME: Sometimes the screen info lies
+			if info.masks[-1] == 0:
+				info.bitsize -= 8
+		else:
+			info.bitsize = surface.get_bitsize()
+			info.masks   = list(surface.get_masks())
+			info.shifts  = surface.get_shifts()
+			info.losses  = surface.get_losses()
+
+		print info
+		self.info = info
+
 		if info.bitsize == 32:
 			if info.masks[ALPHA] == 0:
 				# No alpha (padding byte) 
@@ -97,56 +94,75 @@ class pygameMNG(MNG):
 			raise RuntimeError("Unable to figure out the best format for this video file.")
 
 		print "Detected image format to be", NAMESPERPIXEL[format], sdlformat
-		self.sdlformat = sdlformat
 		MNG.__init__(self, file, format)
-		print self.bitsperpixel, self.buffer_size	
 
-#		def processheader(self, width, height):
-#			print "SDL processheader", self, self.sdlformat
-#			self.initalized = True
-#			# Create a buffer which the library will output to
-#			self.buffer_size = width*height*self.bitsperpixel/8
-#			self.image	 	 = pygame.surface.Surface((width, height), pygame.SRCALPHA)
+	def getcanvasline(self, line):
+		p = self.buffer + (self.width*line*self.bitsperpixel/8)
+		return p
 
-	def nextframe(self):
-#			# Lock the surface
-#			self.image.lock()
-#			print self.getticks(), self.delay
-#			if self.getticks() > self.delay:
-#				print "calling display_resume"
-#				mng.mng_display_resume(self.mng_handle)
-#			self.image.unlock()
-#			return d, self.image
-		d, buffer = MNG.nextframe(self)
-		return d, pygame.image.frombuffer(buffer, self.size, "RGBA") #self.sdlformat)
+	def processheader(self, width, height):
+		self.initalized = True
+
+		# Create a buffer which the library will output to
+		self.image = pygame.surface.Surface((width, height)).convert(self.info.masks)
+		if self.bitsperpixel != self.image.get_bitsize():
+			raise RuntimeError("Created surface (%s) does not have correct bitsize (%s)." % \
+				(self.image.get_bitsize(), self.bitsperpixel))
+		self.buffer = CPygameSurface(self.image).pixels
+
+		MNG.processheader(self, width, height)
+
+	def nextframe(self, force=False):
+		if self.getticks() > self.delay or force:
+			# FIXME: Would memset be faster?
+			self.image.fill((0,0,0,0)) 
+			self.display_resume()
+		return self.delay, self.image
 
 if __name__ == "__main__":
 	import sys
+
 	if len(sys.argv) > 1:
 		import pygame
-		screen = pygame.display.set_mode((640, 480), 16, 32) 
+		screen = pygame.display.set_mode((800, 600), 0, 16) 
 		pygame.display.set_caption('Battle Viewer') 
-		screen.fill((255,255,255))
+		screen.fill((255,0, 0))
 
-		a = pygameMNG(sys.argv[1], screen)
-		print "sigtype\t\t\t",			a.sigtype
-		print "type\t\t\t",				a.type
-		print "width\t\t\t",			a.width
-		print "height\t\t\t",			a.height
-		print "ticks\t\t\t",			a.ticks
-		print "framecount\t\t",			a.framecount
-		print "layercount\t\t",			a.layercount
-		print "playtime\t\t",			a.playtime
-		print "simplicity\t\t",			a.simplicity
-		print "bitdepth\t\t",			a.bitdepth
-		print "colortype\t\t",			a.colortype
-		print "compression\t\t",		a.compression
-		print "filter\t\t\t",			a.filter
-		print "interlace\t\t",			a.interlace
-		print "alphabitdepth\t\t",		a.alphabitdepth
-		print "alphacompression\t",		a.alphacompression
-		print "alphafilter\t\t",		a.alphafilter
-		print "alphainterlace\t\t",		a.alphainterlace
+		p = pygame.image.load("../../temp/barren/barren1_000.png").convert_alpha()
+
+		print
+#		s1 = pygame.surface.Surface((100,100), pygame.SRCALPHA).convert_alpha()
+#		print "Original BitsPerPixel", s1.get_bitsize()
+#		a1 = pygameMNG(sys.argv[1], s1)
+		print "---------------------------------------------------------"
+		print
+#		s2 = pygame.surface.Surface((100,100)).convert()
+#		print "Original BitsPerPixel", s2.get_bitsize()
+#		a2 = pygameMNG(sys.argv[1], s2)
+#		print "---------------------------------------------------------"
+#		print
+		a1 = pygameMNG(sys.argv[1], p)
+#		print "---------------------------------------------------------"
+#		print
+
+		print "sigtype\t\t\t",			a1.sigtype
+		print "type\t\t\t",				a1.type
+		print "width\t\t\t",			a1.width
+		print "height\t\t\t",			a1.height
+		print "ticks\t\t\t",			a1.ticks
+		print "framecount\t\t",			a1.framecount
+		print "layercount\t\t",			a1.layercount
+		print "playtime\t\t",			a1.playtime
+		print "simplicity\t\t",			a1.simplicity
+		print "bitdepth\t\t",			a1.bitdepth
+		print "colortype\t\t",			a1.colortype
+		print "compression\t\t",		a1.compression
+		print "filter\t\t\t",			a1.filter
+		print "interlace\t\t",			a1.interlace
+		print "alphabitdepth\t\t",		a1.alphabitdepth
+		print "alphacompression\t",		a1.alphacompression
+		print "alphafilter\t\t",		a1.alphafilter
+		print "alphainterlace\t\t",		a1.alphainterlace
 
 		def input(events): 
 			for event in events: 
@@ -156,8 +172,12 @@ if __name__ == "__main__":
 					print event 
 
 		while True: 
-			delay, image = a.nextframe()
+			screen.fill((0,255,0))
+
+			delay, image = a1.nextframe()
 			screen.blit(image, (0,0))
+			screen.blit(p, (129, 0))
+
 			pygame.display.flip()
 			input(pygame.event.get())
 
